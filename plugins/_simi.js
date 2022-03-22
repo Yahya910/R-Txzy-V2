9d329ae746a4ea1376331bd959927c8e2c7d57bf
AYANUKOJI-KEN <100865866+Yahya910@users.noreply.github.com>
2022-03-22 13:29:07 +0100

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -13,7 +13,7 @@ handler.before = async (m) => {
         let res = await fetch(API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: 'id' }))
         if (!res.ok) return m.reply(eror)
         let json = await res.json()
-        if (json.success == "Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.") return m.reply('simi nya gk tau bang')
+        if (json.success == "ادفع للحصول على عضوية طرق الدفع غير محددة.") return m.reply('في الخاص 🌺💜')
         m.reply(json.success)
         return !0
     }
