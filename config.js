a1aec5a2b0f1cb2a7e34b061cbf5866edb956890
AYANUKOJI-KEN <100865866+Yahya910@users.noreply.github.com>
2022-03-22 13:25:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285730903853', '6282221792667','6285730903853', '6282221792667', '6285730903853'] // Letakan nomor kamu disini
+global.owner = ['212690762206'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -44,8 +44,8 @@ global.APIKeys = { // APIKey nya disini
 global.packname = 'Exc'
 global.author = 'NyyXz'
 
-global.wait = '「 ⏱️ 」Harap tunggu...'
-global.eror = '「❗」Server ERROR!'
+global.wait = '*「 ⏱️ 」انتضر قليلا...*'
+global.eror = '*「❗」مشكل في سرفر!*'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '© R-Txzy' //change the watermark 
 global.image = 'https://telegra.ph/file/0bdd84bd5a8099a789828.jpg' //change the image
@@ -57,7 +57,7 @@ global.kandang = 'https://telegra.ph/file/67a6ee607d03a4e52757d.jpg'
 global.kolam = 'https://telegra.ph/file/5aa5dfa3394477e11fb18.jpg'
 global.thanks = 'https://telegra.ph/file/01917f7782b70de8f418d.jpg'
 
-global.multiplier = 9999999 // Semakin tinggi, semakin sulit naik level
+global.multiplier = 9999999 // أفضل لفل
 
 let chalk = require('chalk')
 let file = require.resolve(__filename)
